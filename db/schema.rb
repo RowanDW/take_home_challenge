8769f37857bf34703e175e7837e96f89878778c4
Rowan DeLong <rowanwinzer@gmail.com>
2022-01-05 01:05:14 -0700

    Add subscription model, db table, and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_040407) do
+ActiveRecord::Schema.define(version: 2022_01_05_044406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 2022_01_05_040407) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "title"
+    t.float "price"
+    t.string "status"
+    t.bigint "customer_id", null: false
+    t.bigint "tea_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_subscriptions_on_customer_id"
+    t.index ["tea_id"], name: "index_subscriptions_on_tea_id"
+  end
+
   create_table "teas", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -33,4 +45,6 @@ ActiveRecord::Schema.define(version: 2022_01_05_040407) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "subscriptions", "customers"
+  add_foreign_key "subscriptions", "teas"
 end
