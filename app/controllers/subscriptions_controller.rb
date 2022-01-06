ee36b9802066771b298dcbadf0225a8895a9a5f4
Rowan DeLong <rowanwinzer@gmail.com>
2022-01-06 11:26:21 -0700

    Update subscribe endpoint error handling

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,13 +1,26 @@
 class SubscriptionsController < ApplicationController
 
     def create
-        cust = Customer.find_by(id: params[:customer])
-        tea = Tea.find_by(id: params[:tea])
-        sub = Subscription.new(title: params[:title], price: params[:price], status: 'Active', customer: cust, tea: tea)
-        if sub.save
+        begin
+            cust = Customer.find(params[:customer])
+            tea = Tea.find(params[:tea])
+            sub = Subscription.create!(title: params[:title], price: params[:price], status: 'Active', customer: cust, tea: tea)
             render json: SubscriptionSerializer.subscription(sub), status: 200
-        else
-            render json: { "error": sub.errors.full_messages.join(", ") }, status: 400
+        rescue ActiveRecord::RecordInvalid => error
+            render json: { "error": error.message }, status: 400
+        rescue ActiveRecord::RecordNotFound => error
+            render json: { "error": error.message }, status: 400
         end
     end
+
+    # def create
+    #     cust = Customer.find_by(id: params[:customer])
+    #     tea = Tea.find_by(id: params[:tea])
+    #     sub = Subscription.new(title: params[:title], price: params[:price], status: 'Active', customer: cust, tea: tea)
+    #     if sub.save
+    #         render json: SubscriptionSerializer.subscription(sub), status: 200
+    #     else
+    #         render json: { "error": sub.errors.full_messages.to_sentence }, status: 400
+    #     end
+    # end
 end 
--- spec/requests/subscribe_spec.rb
+++ spec/requests/subscribe_spec.rb
@@ -29,12 +29,12 @@ RSpec.describe 'subscribe customer endpoint' do
         expect(response).to_not be_successful
 
         sub = JSON.parse(response.body, symbolize_names: true)
-        expect(sub[:error]).to eq("Price can't be blank")
+         expect(sub[:error]).to eq("Validation failed: Price can't be blank")
 
         post '/subscriptions', params: {customer: 1, tea: @tea.id, title: "Sub 1", price: 300}
         expect(response).to_not be_successful
 
         sub = JSON.parse(response.body, symbolize_names: true)
-        expect(sub[:error]).to eq("Customer can't be blank, Customer must exist")
+        expect(sub[:error]).to eq("Couldn't find Customer with 'id'=1")
     end
 end
