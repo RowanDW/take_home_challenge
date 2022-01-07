f5c1ffb394de55820a69e92174e655f89003e4eb
Rowan DeLong <rowanwinzer@gmail.com>
2022-01-06 21:29:27 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+rowan = Customer.create!(first_name: "Rowan", last_name: "DeLong", email: "rowan@test.com", address: "123 st")
+kovu = Customer.create!(first_name: "Kovu", last_name: "The Cat", email: "kovu@test.com", address: "123 st")
+
+oolong = Tea.create!(title: "Oolong", description: "A type of tea", temperature: 180, brew_time: 150)
+jasmine = Tea.create!(title: "Jasmine", description: "A green tea", temperature: 160, brew_time: 150)
+
+Subscription.create!(title: "Rowans oolong subscription", price: 35.99, tea: oolong, customer: rowan)
